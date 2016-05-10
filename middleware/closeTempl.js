16367ae958d6acc5551677938da34413cdce617d
Carlos Rodriguez <carlos@s8f.org>
2016-05-10 10:11:52 -0700

    use new console

--- middleware/closeTempl.js
+++ middleware/closeTempl.js
@@ -2,9 +2,9 @@ module.exports = function container (get, set) {
   return function closeTempl (cb) {
     var templ_instance = get('middleware.templ_instance')
     if (templ_instance) {
-      get('vendor.console').log('motley-templ: closing templ.')
+      get('console').log('motley-templ: closing templ.')
       templ_instance.close()
-      get('vendor.console').log('motley-templ: closed templ.')
+      get('console').log('motley-templ: closed templ.')
       setImmediate(cb)
     }
     else setImmediate(cb)
